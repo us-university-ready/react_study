a4c7c8c29441b151f885fb7715dfb049774bb34e
Gyeongsik Kim <gyungdal@the-s.kr>
2019-09-02 13:42:53 +0900

    현재 기기의 속성 표시하도록 추가

--- src/views/components/sample/Sample.tsx
+++ src/views/components/sample/Sample.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
+import { versions, arch } from "process";
 
 export interface SampleProps { compiler: string; framework: string; library: string; }
 
 export class Sample extends React.Component<SampleProps, {}> {
+    constructor(props: SampleProps) {
+        super(props);
+        props.compiler = `Node ${versions.node}`;
+        props.framework = `${versions.v8}`;
+        props.library = `${versions.modules}`;
+    }
+    
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework} and {this.props.library}!</h1>;
     }
